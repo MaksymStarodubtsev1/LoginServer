f323096feac9e1ba51f8ff93eb2ed30f5d282b3c
victor1starodubtsev <victor1starodubtsev@gmail.com>
2022-03-12 17:46:05 +0200

    server test

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 axios.post('http://localhost:3000/book', {
-
+name: 'NewServer'
 })
 .then(function (response) {
   console.log(response.data,{name: 'lol'});
--- server.js
+++ server.js
@@ -15,8 +15,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
-    const name = req.name
-    res.send('Book is added to the database' + name);
+    const name = 'name'
+    console.log('HELLO')
+
+    res.send(
+        {
+        description: `${Object.keys(req)}`,
+        value: books
+    });
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
